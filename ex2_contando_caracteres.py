8e1b7d8b8c9e2832a6551519eb60488f306ec5a1
Edmilton Leonel <118381886+Edmilton-Leonel@users.noreply.github.com>
2025-09-03 18:14:09 +0200

    Resolução do exercício
    Resolução do exercício, aparentemente sem nenhuma possível falha.

--- ex2_contando_caracteres.py
+++ ex2_contando_caracteres.py
@@ -7,3 +7,38 @@ contagem de cada caractere. Por exemplo, se a string for
 "banana", a função deve retornar {'b': 1, 'a': 3, 'n': 2}.
 
 """
+
+
+def contar_caracteres(string: str) -> dict[str, int]:
+
+    caracteres_contados: dict[str, int] = dict()
+
+    for caracter in string:
+
+        if caracteres_contados.get(caracter):
+
+            repeti_caracter = caracteres_contados[caracter]
+            caracteres_contados[caracter] = repeti_caracter + 1
+
+        else:
+            caracteres_contados[caracter] = 1
+
+    return caracteres_contados
+
+
+def executar_programa() -> None:
+    while True:
+        texto = str(input(
+            'Digite uma cadeia de caracteres para contar.\n'
+            '0 -> para sair\n\t\t'
+        ))
+
+        if texto == '0':
+            print('Fim da execução.\n')
+            break
+
+        contagem = contar_caracteres(texto)
+        print(f'\n{contagem}', end='\n\n')
+
+
+executar_programa()
